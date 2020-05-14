fed28fe6602042dc9e5a31b0d9ae23873bab26dc
Saurabh Taware <saurabh.taware@cimcorp.com>
2020-05-14 15:24:16 +0300

    Loading symbol added, added boxes for search - not implemented

--- src/components/List.js
+++ src/components/List.js
@@ -10,60 +10,110 @@ import { useHistory } from 'react-router-dom'
 import Dropdown from 'react-bootstrap/Dropdown'
 import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
+import Form from 'react-bootstrap/Form';
+
+import Moment from 'react-moment';
+import Spinner from 'react-bootstrap/Spinner'
 
 
-
+//const testing  = axios.get('http://localhost:8080/api/activeBookings')
+//todo - add search sort and
 
 function List() {
+
+  const[state,setState] = useState({
+    bookingList:[],
+    loading:true,
+    searchString:'',
+    pageRows:10,
+    
+  })
   const history = useHistory();
   
-  const [bookingList, setBookingList] = useState([])
   useEffect(() => {
-    fetchBookingList()    
+       
+    axios.get('http://localhost:8080/api/activeBookings').then(response=>{
+        console.log(response.data)
+        setState(prevState => ({
+          loading:false,
+          bookingList:[...response.data]
+        }))
+      }).catch(error => {
+        console.log(error)
+      })        
   }, [])
 
-  const fetchBookingList = async()=>{    
-    axios.get('http://localhost:8080/api/activeBookings')
-    .then(response => {    
-      console.log(response.data)  
-      setBookingList(response.data)
-    }).catch(error => {
-      console.log(error)
-    })
-  }
+  
+  // const fetchBookingList = async () => {    
+  //   getData.then(response => {
+  //       console.log(response.data)
+  //       setState({
+  //         bookingList: [...response.data].filter((booking)=>{
+  //           if(state.searchString){
+  //             return booking.firstName.toLowerCase().includes(state.searchString.toLowerCase())
+  //           }else{
+  //               return true
+  //           } 
+  //         }),
+  //         loading:false
+  //       })               
+  //     }).catch(error => {
+  //       console.log(error)
+  //     })
+  // }
 
   const handleStatus = (id, status) => {
-    let statusToBeupdated = 0    
-    if(status===1){
+    let statusToBeupdated = 0
+    if (status === 1) {
       statusToBeupdated = 2
-    }else if(status===2){
+    } else if (status === 2) {
       statusToBeupdated = 1
-    }    
-    axios.put(`http://localhost:8080/api/updateStatus/${id}/${statusToBeupdated}`)      
-    .then(response => {    
-      console.log(response.data)  
-      setBookingList(response.data)
-    }).catch(function (error) {
+    }
+    axios.put(`http://localhost:8080/api/updateStatus/${id}/${statusToBeupdated}`)
+      .then(response => {
+        console.log(response.data)
+        setState({
+          bookingList: [...response.data],
+          loading:false
+        })
+      }).catch(function (error) {
         console.log(error)
-      }) 
-    
+      })
+
   }
 
-  const handleEdit=(record)=>{    
-    history.push('/form',{record:record})
+  const handleEdit = (record) => {
+    history.push('/form', { record: record })
 
   }
 
   const handleDelete = (id) => {
-    axios.put(`http://localhost:8080/api/inactivateBooking/${id}`)      
-    .then(response => {    
-      console.log(response.data)  
-      setBookingList(response.data)
-    }).catch(function (error) {
+    axios.put(`http://localhost:8080/api/inactivateBooking/${id}`)
+      .then(response => {
+        console.log(response.data)
+        setState({
+          bookingList: [...response.data],
+          loading:false
+        })
+      }).catch(function (error) {
         console.log(error)
-      }) 
-    
+      })
 
+
+  }
+
+  const handleRowChange = (event) => {    
+    // setState({
+    //   pageRows:event.target.value
+    // })
+   
+  }
+
+  const handleSearch=(event)=>{    
+    // setState({
+    //   searchString: event.target.value.toLowerCase()      
+    // })
+   
   }
 
   const TableRow = (props) => {
@@ -77,20 +127,22 @@ function List() {
         <td>{props.record.firstName}</td>
         <td>{props.record.lastName}</td>
         <td>{props.record.email}</td>
+        <td> <Moment format="MM/DD/yyyy">{props.record.startDate}</Moment></td>
+
         <td>
           <Dropdown as={ButtonGroup}>
 
-            {<Button variant={props.record.status === 2 ? "danger" : "info"} 
-                     onClick={() => handleStatus(props.record.appointmentId, props.record.status)}>
-                     {props.record.status === 2 ? "Remove confirmation" : "Confirm"} 
-              </Button>}
+            {<Button variant={props.record.status === 2 ? "danger" : "info"}
+              onClick={() => handleStatus(props.record.appointmentId, props.record.status)}>
+              {props.record.status === 2 ? "Remove confirmation" : "Confirm"}
+            </Button>}
 
 
             <Dropdown.Toggle split variant={props.record.status === 2 ? "danger" : "info"} id="dropdown-split-basic" />
 
             <Dropdown.Menu>
-              <Dropdown.Item onClick={()=>handleEdit(props.record)}>Edit</Dropdown.Item>
-              <Dropdown.Item onClick={()=>handleDelete(props.record.appointmentId)}>Delete</Dropdown.Item>              
+              <Dropdown.Item onClick={() => handleEdit(props.record)}>Edit</Dropdown.Item>
+              <Dropdown.Item onClick={() => handleDelete(props.record.appointmentId)}>Delete</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
 
@@ -99,6 +151,10 @@ function List() {
     )
   }
 
+ 
+
+
+
 
   return (
     <Container fluid>
@@ -106,41 +162,67 @@ function List() {
       <Row>
         <Col><h4 className="font-weight-normal">Booking List</h4></Col>
       </Row>
-      <div>
-        <Row></Row>
-        <Row>
+      {state.loading && <Spinner animation="grow" />}
+     
+      {!state.loading &&
+      <Row>
+        <Form>
+          <Form.Row>            
+            <Form.Group as={Col} controlId="rowSelections">
+              <Form.Label>Rows</Form.Label>
+              <Form.Control as="select" size="md" custom onChange={handleRowChange}>
+                <option>10</option>
+                <option>20</option>
+                <option>30</option>
+                <option>40</option>
+                <option>50</option>
+              </Form.Control>
+            </Form.Group>
 
-          <Table hover responsive>
-            <thead>
+            <Form.Group as={Col} controlId="textSearch">
+              <Form.Label>Text Search</Form.Label>
+              <Form.Control name="textSearch" placeholder="Search.." value={state.searchString} onChange={handleSearch}/>
+            </Form.Group>
 
-              <tr><th>#</th>
-                <th >Reference</th>
-                <th >First Name</th>
-                <th >Last Name</th>
-                <th>Email</th>
-                <th>Actions</th>
+            <Form.Group as={Col} controlId="startDate">
+              <Form.Label>Start Date</Form.Label>
+              <Form.Control type="date" name="startDate" />
+            </Form.Group>
 
-              </tr>
-            </thead>
-            <tbody>
-              {
-                bookingList.map((record, index) => (
-                  <TableRow key={index} record={record} index={index}></TableRow>
+            <Form.Group as={Col} controlId="endDate">
+              <Form.Label>End Date </Form.Label>
+              <Form.Control type="date" name="endDate" />
+            </Form.Group>
+          </Form.Row>
+        </Form>
 
-                ))
+      </Row>
+      }
 
-              }
+      <Row>
 
+        <Table hover responsive>
+          <thead>
+            <tr>
+            <th>#</th>
+              <th >Reference</th>
+              <th >First Name</th>
+              <th >Last Name</th>
+              <th>Email</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              state.bookingList.map((record, index) => (
+                <TableRow key={index} record={record} index={index}></TableRow>
 
-            </tbody>
-          </Table>
-
-
-
-        </Row>
-        <Row></Row>
-      </div>
-
+              ))
+            }
+          </tbody>
+        </Table>
+      </Row>
 
     </Container>
   );
